dc653dfdfbd90b3f82e98cdf9a7c6d564e001155
Themba <themba.dombo@gmail.com>
2020-04-23 11:14:21 +0200

    removed some comments and order code better

--- spec/support/simple_calculatro.spec.js
+++ spec/support/simple_calculatro.spec.js
@@ -4,39 +4,26 @@ var mathFunction = require('/home/haplo/Documents/Umuzi/Techdep/SimpleCalc/src/s
 
 describe('Addition Math', function(){
     it("expects two positive numbers to be added", function(){
-        const num1 = 2;
-        const num2 = 1;
         //act
-        const results = mathFunction.add(num1,num2);
+        const results = mathFunction.add(1,2);
         // arrange
         expect(results).toBe(3)
-
-
     })
 
     it("expects two negative numbers to be added", function(){
         const num1 = -1;
         const num2 = -1;
         //act
-        const results = mathFunction.add(num1,num2);
+        const results = mathFunction.add(-1,-1);
         // arrange
         expect(results).toBe(-2)
-
-
     })
 
     it("expects more than two numbers to be added together", function(){
-        const num1 = 1;
-        const num2 = 2;
-        const num3= 3;
-        const num4 = 4;
-        const num5= 5;
         //act
-        const results = mathFunction.add(num1,num2,num3,num4,num5);
+        const results = mathFunction.add(1,2,3,4,5);
         // arrange
         expect(results).toBe(15)
-
-
     })
 });
 
@@ -44,44 +31,24 @@ describe('Addition Math', function(){
 
 describe('Multiplication Math', function(){
     it("expects two positive numbers to be multiplied together", function(){
-        const num1 = 3;
-        const num2 = 1;
         //act
-        const results = mathFunction.multiply(num1,num2);
+        const results = mathFunction.multiply(1,3);
         // arrange
         expect(results).toBe(3)
-
-
     })
 
     it("expects a positive numbers to be multiplied by a negative number", function(){
-        const num1 = 3;
-        const num2 = -1;
         //act
-        const results = mathFunction.multiply(num1,num2);
+        const results = mathFunction.multiply(-1,3);
         // arrange
         expect(results).toBe(-3)
-
-
     })
 
     it("expects to multiply more than two numbers", function(){
-        const num1 = 1;
-        const num2 = 2;
-        const num3= 3;
-        const num4 = 4;
-        const num5= 5;
         //act
-        const results = mathFunction.multiply(num1,num2,num3,num4,num5);
+        const results = mathFunction.multiply(1,2,3,4,5);
         // arrange
         expect(results).toBe(120)
-
-
     })
 });
-    //mutilply(1,2,3,4,5)
-    // should return 120
-    //multiply(1,3)
-    // should still return 3
-    //multiply(-1,3)
-    // should still return -3
+    
--- src/simpleCalculator.js
+++ src/simpleCalculator.js
@@ -1,5 +1,4 @@
 function add (){
-
     var sum = 0;
     for (i = 0; i < arguments.length; i++) {
         sum += arguments[i];
@@ -10,7 +9,6 @@ function add (){
     
 
 function multiply(){
-   
     var multiplied = 1;
     for (i = 0; i < arguments.length; i++) {
         multiplied = multiplied*arguments[i];
